fe598f7f58727e035e7a93572a9ebe8225e7d622
Dan Nguyen <dansonguyen@gmail.com>
2015-09-22 04:56:39 -0700

    date helper

--- lib/middleman-onthestreet/helpers/onthestreet_helpers.rb
+++ lib/middleman-onthestreet/helpers/onthestreet_helpers.rb
@@ -1,5 +1,6 @@
 
-require_relative './page_data_helpers'
+require_relative './date_helpers'
 require_relative './meta_helpers'
 require_relative './markup_helpers'
 require_relative './media_helpers'
+require_relative './page_data_helpers'
